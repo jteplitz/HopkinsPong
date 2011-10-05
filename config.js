3071490ac7d7838655c876fdfa4f078550ea236a
Jason Teplitz <jteplitz602@gmail.com>
2011-10-05 19:41:05 -0400

    Possibly fixed crashing issue related to mongo on heroku

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-module.exports.databaseURI = "mongodb://hopkinscsc:notsony@dbh54.mongolab.com:27547/heroku_app1258772/users";
+module.exports.databaseURI = "mongodb://hopkinscsc:notsony@dbh54.mongolab.com:27547/heroku_app1258772";
